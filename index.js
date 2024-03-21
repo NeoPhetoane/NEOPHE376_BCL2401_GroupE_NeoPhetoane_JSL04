64605021c520a380673744a9af3658a50159e03b
NeoPhetoane <phetoaneneo4@gmail.com>
2024-03-21 22:45:19 +0200

    Challenges Complete

--- index.js
+++ index.js
@@ -57,21 +57,40 @@ console.log(heartRate < 100 ? "Boost needed!" : "Energy levels are high!");
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!"
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
+/* If/else
+
+let currentTemp = 4;
+
+if (currentTemp <= 5) {
+    console.log("Chilled to perfection!");
+} else {
+    console.log("Needs a cooler!")
+}
+*/
+
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+console.log(currentTemp <= 5 ? `Chilled to perfection!` : `Needs a cooler!`);
 
 // #### Challenge 5: Late Night Coding Session
 // Determine if it's a good idea to have a Monster Energy drink based on the current hour.
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
 // otherwise, print "Better stick to water."
 
+/*If/else
+
+let currentHour = 22
+
+if (currentHour >= 7 && currentHour < 24) {
+    console.log('Unleash the beast!');
+} else {
+    console.log('Better stick to water.');
+}
+*/
+
 // Define the current hour in 24-hour format
 let currentHour = 22;
-
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
-
-// In each of these challenges, the ternary operator is used to decide between two options based on a condition,
-// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct,
-// they should be used judiciously to maintain readability, especially for more complex conditions.
+console.log(
+  currentHour >= 7 && currentHour < 24
+    ? "Unleash the beast!"
+    : "Better stick to water."
+);
